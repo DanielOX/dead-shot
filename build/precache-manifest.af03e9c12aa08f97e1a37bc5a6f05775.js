2036be6b277a5949d136c1b6242d176a058928e4
Daniel Malik <metalcool07@yahoo.com>
2019-04-06 20:57:20 +0500

    Dead Shot Initial Commit

---
+++ build/precache-manifest.af03e9c12aa08f97e1a37bc5a6f05775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96624d308a0da714a4b56af386139bff",
+    "url": "/static/media/logo.96624d30.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eef731c59734ea0268b6",
+    "url": "/static/js/main.b3ccc7cf.chunk.js"
+  },
+  {
+    "revision": "02b2da75844a98bc94b8",
+    "url": "/static/js/2.8438b307.chunk.js"
+  },
+  {
+    "revision": "eef731c59734ea0268b6",
+    "url": "/static/css/main.36ab7b5f.chunk.css"
+  },
+  {
+    "revision": "498c0ac9cd282a8ed37dad5f99e2d600",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Card, Container } from "semantic-ui-react";
+import Search from "./Components/Search";
+import ServerVulnerability from "./Components/ServerVulnerability";
+import WebTechnologies from "./Components/WebTechnologies";
 
+import "./App.css";
+import DeadShotLogo from "./logo.png";
+const width = {
+  container: {
+    width: "70%",
+    margin: "0 auto"
+  },
+  card: {
+    width: "100%"
+  }
+};
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container style={width.container}>
+        <div className="padder">
+          <center>
+            <img src={DeadShotLogo} alt="" />
+            <br />
+            <p style={{ fontSize: "18px" }}>
+              Reconnaissance Your Site & Server
+            </p>
+          </center>
+        </div>
+        <br />
+        <Search />
+        <Card style={width.card}>
+          <Card.Content>
+            <ServerVulnerability />
+          </Card.Content>
+        </Card>
+
+        <Card style={width.card}>
+          <Card.Content>
+            <WebTechnologies />
+          </Card.Content>
+        </Card>
+      </Container>
     );
   }
 }
